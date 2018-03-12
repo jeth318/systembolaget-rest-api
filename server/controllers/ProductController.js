1e5a1c66081539f49d8f7782e8dbdf21d4d07bbc
jeth318 <jesper.thornberg@me.com>
2018-03-12 22:05:03 +0100

    wip

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -21,7 +21,7 @@ function GetAll(req, res) {
 function GetCustom(req, res) {
 
     let customQuery = {};
-    Object.keys(req.body).forEach(function(prop) {
+    Object.keys(req.body).forEach(function (prop) {
         customQuery[prop] = new RegExp(".*" + req.body[prop] + ".*");
     });
 
@@ -29,13 +29,12 @@ function GetCustom(req, res) {
         if (err) return res.status(500).send("There was a problem finding the products.");
         res.status(200).send(products);
     })
-    .limit(req.body["limit"] ? parseInt(req.body["limit"]) : null);
-    
+        .limit(req.body["limit"] ? parseInt(req.body["limit"]) : null);
+
 }
 
 // GETS A SINGLE USER IN THE DATABASE
 function GetOne(req, res) {
-    console.log(req);
     Product.findById(req.params.id, function (err, product) {
         if (err) return res.status(500).send("There was a problem finding the product.");
         if (!product) return res.status(404).send("No product found.");
@@ -44,10 +43,10 @@ function GetOne(req, res) {
 }
 
 // DELETES A PRODUCT FROM THE DATABASE
-function Remove (req, res) {
+function Remove(req, res) {
     Product.findByIdAndRemove(req.params.id, function (err, product) {
         if (err) return res.status(500).send("There was a problem deleting the product.");
-        res.status(200).send("Product: "+ product.name +" was deleted.");
+        res.status(200).send("Product: " + product.name + " was deleted.");
     });
 }
 
@@ -55,46 +54,49 @@ function Remove (req, res) {
 // UPDATES ALL PRODUCTS IN THE DATABASE
 function Update(req, res) {
 
-    const url = `https://www.systembolaget.se/api/assortment/products/xml`
-    let firstLevel = 'artiklar';
-    let secondLevel = 'artikel';
-    let backup = {};
+const url = `https://www.systembolaget.se/api/assortment/products/xml`
+let firstLevel = 'artiklar';
+let secondLevel = 'artikel';
+let backup = {};
 
-    var p1 = Product.find({});
-    var p2 = rp(url);
-    var p3 = Hash.findOne({'type': 'products'})
-    var p4 = Product.remove({});
+var p1 = Product.find({});
+var p2 = rp(url);
+var p3 = Hash.findOne({ 'type': 'products' })
+var p4 = Product.remove({});
+var oldProducts;
+var newProducts;
+var storedHash;
+var parsedXml;
 
-    Promise.all([p1,p2,p3])
-    .then((data)=>{
-        const oldProducts = data[0];
-        const newProducts = data[1];
-        const storedHash  = data[2];
-
-        if(sha(newProducts) === storedHash){
-            return res.status(200).send('No need to update!');
-        } else {
-            console.log('RP STARTED')
-            return rp(url);
-        }
+Promise.all([p1, p2, p3, p4])
+  .then((data) => {
+    console.log('All initial promises resolved')
+    oldProducts = data[0];
+    newProducts = data[1];
+    storedHash = data[2].hash;
+    
+    const options = { explicitArray: false, normalizeTags: true, attrkey: "attr" };            
+    return new Promise((resolve, reject) => {
+    console.log('Parsing XML...')
+      parseString(newProducts, options, function (err, jsonParsed) {
+          console.log('Done parsing XML...')
+          
+          err ? reject(err) : resolve(jsonParsed[firstLevel][secondLevel])
+      });
     })
-    .then((xmlString)=>{
-        const options = { explicitArray: false, normalizeTags: true, attrkey: "attr" };
-        return new Promise((resolve, reject)=>{
-            parseString(xmlString, options, function(err, jsonParsed) {
-                err ? reject(err) : resolve(jsonParsed[firstLevel][secondLevel])
-              });
-        })
-        
-    })
-    .then((parsedXml)=>{
-        return Product.insertMany(parsedXml);
-    })
-    .then(()=>{
-        return Hash.findOneAndUpdate({'type': 'products'}, { hash: sha(newProducts) })
-    })
-    .then(()=>res.status(200).send('Update complete!'))
-    .catch((err)=>res.status(500).send("There was a problem updating the products. " + err))    
+  })
+  .then((result) => {
+    parsedXml = result;
+    console.log('Checking hash...')
+    if (sha(parsedXml) === storedHash) {
+      return 'No need to update';
+    } else {
+      return Product.insertMany(parsedXml);
+    }
+  })
+  .then((message) => {console.log('Hej1'); return Hash.findOneAndUpdate({ 'type': 'products' }, { hash: sha(parsedXml) })})
+  .then((message) => {console.log('Hej2'); return res.status(200).send(message)})
+  .catch((err) => res.status(500).send("There was a problem updating the products. " + err))
 }
 
-module.exports = { GetAll, GetOne, Remove, Update, GetCustom, }
+module.exports = { GetAll, GetOne, Remove, Update, GetCustom }
