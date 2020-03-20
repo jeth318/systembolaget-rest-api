0e77d8f54b884f89e9290dd779aababc76fbca37
Jesper Thörnberg <jesper.thornberg@me.com>
2020-03-20 21:54:10 +0100

    Fix audit

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
  var db = require('./config').db;
 //var db = 'localhost:8080/jwt-token-test';
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>console.log('Connected to: ' + db ))
 .catch((err)=>console.error('Failed to connect to mongoose... ERROR: ' + err))
