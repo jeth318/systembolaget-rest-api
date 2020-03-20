cd63c1f02b240d6e94e96b6d8d1d18f6f2c520e5
Jesper Thörnberg <jesper.thornberg@me.com>
2020-03-20 22:32:00 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+BRANCH_NAME=$1;
+
+if [[ $BRANCH_NAME == "master" ]]; then
+	cd /home/pi/Apps/systembolaget-rest-api
+else
+	exit 0;
+fi
+
+echo "Stashing lock files "
+git stash
+
+echo "Dropping stash"
+git stash drop
+
+echo "Pulling from ${BRANCH_NAME}"
+git pull origin ${BRANCH_NAME}
+echo "Pulled successfully"
+
+echo "Removing node_modules"
+rm -rf node_modules
+echo "Reinstalling dependencies"
+npm install
+echo "Project dependencies was installed"
+
+echo "Rebooting systembolaget-rest-api"
+
+if [[ $BRANCH_NAME == "master" ]]; then
+	echo "pm2 restart systembolaget-rest-api"
+else
+    exit 0;
+fi
+echo "Reboot OK"
+echo "Deployment complete, and it was a success!"
+
+exit 0
